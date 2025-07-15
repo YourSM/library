e2216b89090561e171b5cf2fb2c0ced978107b51
YourSM <156563558+YourSM@users.noreply.github.com>
2025-07-14 17:29:28 -0700

    Add text to book display

--- javascript.js
+++ javascript.js
@@ -15,11 +15,36 @@ function Book(uuid, author, title, pages, hasRead) {
 
 function addBookToLibrary(author, title, pages, hasRead) {
   myLibrary.push(new Book(crypto.randomUUID(), author, title, pages, hasRead))
-  displayBooks()
+  addBookInfo(author, title, pages, hasRead)
 }
 
-function displayBooks() {
+function displayBooks(bookInfo) {
     const book = document.createElement("div")
     book.classList.add("book")
     libraryContainer.appendChild(book)
+    book.appendChild(bookInfo)
+}
+
+  function addBookInfo(author, title, pages, hasRead) {
+
+    const bookInfo = document.createElement("div")
+    bookInfo.classList.add("bookInfo")
+    
+    authorContainer = document.createElement("p")
+    authorContainer.textContent = `Author: ${author}`;
+    bookInfo.appendChild(authorContainer);
+
+    titleContainer = document.createElement("p")
+    titleContainer.textContent = `Title: ${title}`;
+    bookInfo.appendChild(titleContainer);
+
+    pagesContainer = document.createElement("p")
+    pagesContainer.textContent = `Pages: ${pages}`;
+    bookInfo.appendChild(pagesContainer);
+
+    hasReadContainer = document.createElement("p")
+    hasReadContainer.textContent = `Read: ${hasRead}`;
+    bookInfo.appendChild(hasReadContainer);
+    
+    displayBooks(bookInfo)
   }
